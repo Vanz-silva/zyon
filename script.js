559304a5876913e1b9bd0325560665e8f6b6e002
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2026-02-27 18:14:30 -0300

    chore: 1 musica

--- script.js
+++ script.js
@@ -1,36 +1,4 @@
-console.log("Site carregado");
-
-// Lista de músicas
-
-const playlist = ["zJpWCTbgeOA", "zBUurckfIiE", "5uB-RfBhhok", "7Bkal8xj_h0"];
-
-let current = 0;
-
-// Botão tocar
-
 document.getElementById("playMusic").onclick = function () {
-  playMusic();
-};
-
-// Função tocar
-
-function playMusic() {
   document.getElementById("player").src =
-    "https://www.youtube.com/embed/" + playlist[current] + "?autoplay=1";
-
-  // duração média da música (4 minutos)
-
-  setTimeout(nextMusic, 240000);
-}
-
-// Próxima música
-
-function nextMusic() {
-  current++;
-
-  if (current >= playlist.length) {
-    current = 0;
-  }
-
-  playMusic();
-}
+    "https://www.youtube.com/embed/zJpWCTbgeOA?autoplay=1&loop=1&playlist=zJpWCTbgeOA";
+};
