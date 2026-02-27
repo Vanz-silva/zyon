4f3443a1b921c1b032ae3a3cbbe820dc6fa56754
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2026-02-27 18:21:45 -0300

    fix: git add

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
 document.getElementById("playMusic").onclick = function () {
   document.getElementById("player").src =
-    "https://www.youtube.com/embed/zJpWCTbgeOA?autoplay=1&loop=1&playlist=zJpWCTbgeOA";
+    "https://www.youtube.com/watch?v=zJpWCTbgeOA&list=RDzJpWCTbgeOA&start_radio=1";
 };
