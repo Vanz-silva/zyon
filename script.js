918671963f616ab043bfbb6a3d0fedf773c4ef0f
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2026-02-27 09:29:41 -0300

    projeto Zyon 1 commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// arquivo pronto para scripts futuros
+
+console.log("Site carregado");
+document.getElementById("playMusic").onclick = function () {
+  document.getElementById("player").src =
+    "https://www.youtube.com/embed/zJpWCTbgeOA?autoplay=1&loop=1&playlist=zJpWCTbgeOA";
+};
